c9baaa9debe9e6d5e6c12a449767404479f8a57d
Angelsalazar23 <angelitosalazar230597@gmail.com>
2022-10-08 22:05:17 -0500

    Terminado

---
+++ cadenasString.js
@@ -0,0 +1,34 @@
+
+
+
+let nombre='Angel'
+let apellido='Salazar'
+let estudiante= `${nombre}   ${apellido}  `
+
+
+let estudianteMayus= estudiante.toUpperCase()
+let estudianteMinus=estudiante.toLowerCase()
+let numCarac= estudiante.length
+let prrimeraLetra=nombre[0]
+let ultimaLetra=apellido[apellido.length-1]
+let eliminaEspacios= estudiante.trim()
+
+
+
+
+
+let a=true;
+console.log(estudiante.indexOf("nombre"))
+
+
+if(estudiante.indexOf(nombre)===0){
+
+
+
+console.log(a)
+
+}else{
+
+    a=false
+    console.log(a)
+}
